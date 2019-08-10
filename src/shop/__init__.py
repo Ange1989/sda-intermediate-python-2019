fbec050537b13420bc3eaff19b7d7bec9dfa660d
Ange1989 <annadziedziela@gmail.com>
2019-08-10 12:14:03 +0200

    exceptions - shop

--- src/shop/__init__.py
+++ src/shop/__init__.py
@@ -1,12 +1,25 @@
 import typing
 
 from src.shop.customer import Customer
+from src.shop.exceptions import ItemValueNotPositive
+from src.shop.item import Item
 from src.shop.order import Order
 
 
 def notify_everyone(orders: typing.List[Order]):
     for order in orders:
         order.customer.notify()
+
+
+def place_order(customer: Customer, *items: Item) -> Order:
+    order = Order(customer)
+    for item in items:
+        try:
+            order.add_item(item)
+        except ItemValueNotPositive as error:
+            # print("you must provide a positive quantity")
+            print(error.message)
+    return order
 
 
 if __name__ == "__main__":
